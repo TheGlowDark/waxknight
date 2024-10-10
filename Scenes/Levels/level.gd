1cddbaaeba0f7519cb8be8038ee90ebf356fbb89
BKamol <kamoljon2004mm@gmail.com>
2024-10-10 21:17:18 +0300

    Changed cutscenes

--- Scenes/Levels/level.gd
+++ Scenes/Levels/level.gd
@@ -4,9 +4,7 @@ extends Node2D
 @onready var cutscene_sprite = $Cutscenes/Sprite2D
 @onready var camera = $Camera2D
 @onready var cutscene_animator = $Cutscenes/AnimationPlayer
-@onready var cutscene_marker = $Cutscenes/Marker2D
 @onready var canvas_modulate = $CanvasModulate
-
 
 
 func _ready():
@@ -15,13 +13,12 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("restart"):
 		GameManager.load_game()
-	
 
 
 func _on_cutscene_area_body_entered(body):
 	if body is Player:
 		player.hide_ui()
 		camera.target = null
-		camera.global_position = cutscene_marker.global_position
+		camera.global_position = cutscene_sprite.global_position - Vector2(20, 0)
 		canvas_modulate.visible = false
 		cutscene_animator.play("cutscene", -1, 0.1)
